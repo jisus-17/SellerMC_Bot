6dbce6ecd4445eecab26553099dffe6b0266bf61
jisus-17 <77003452+jisus-17@users.noreply.github.com>
2021-01-26 03:16:42 +0100

    m

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 import discord, asyncio
+import json
 
 pre = '#'
 game = discord.Game("Shopping")
 client = discord.Client()
 
+
+with open('SellerMC_Bot/local/token.json') as f:
+    data = json.load(f)
+
+with open('SellerMC_Bot/data/img/logo.png', 'rb') as i:
+    picture = discord.File(i)
+    
+
+token = data['token']
 
 embed1=discord.Embed(color=0x5cd6ff)
 embed1.set_thumbnail(url='https://minecraft-api.com/api/skins/Destine_ice/head/10/10.5/12')
@@ -18,7 +28,7 @@ embed1.set_footer(text='BOTShop ver 0.1.0')
 @client.event
 async def on_ready():
     print('Iniciado como {0.user}'.format(client))
-    await client.change_presence(status=discord.Status.idle, activity=game)
+    await client.change_presence(status=discord.Status.online, activity=game)
 
 
 #@client.event
@@ -34,6 +44,10 @@ async def on_message(message):
     if message.content.startswith(pre + 'ping'):
         await message.channel.send('pong🏓')
 
+    if message.content.startswith(pre + 'user'):
+        #await message.channel.send(picture)
+        await message.channel.send(file=discord.File('SellerMC_Bot/data/img/logo.png'))
+
     if message.content.startswith(pre + 'comprar'):
         
         await message.channel.send(embed=embed1)
@@ -44,4 +58,4 @@ async def on_message(message):
 async def on_reaction_add(reaction, user):
     await reaction.message.channel.send('comprado')
 
-client.run('token')
+client.run(token)
